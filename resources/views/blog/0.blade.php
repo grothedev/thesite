91bee621ec07d5bfadf389aaadb37260fd634319
grothedev <grothedev@gmail.com>
2020-12-11 22:40:33 -0500

    first blog post

--- resources/views/blog/0.blade.php
+++ resources/views/blog/0.blade.php
@@ -8,29 +8,38 @@
 		<h2>To Justify the Absurd</h2>
 		<h2>Semantic Interface of Sentience</h2>
 		<h2>Defining what is</h2>
-		<h6>Placeholder name for the blog</h6>
+		<h6>Placeholder name for the blog. These are just ideas, I need to come up with a name.</h6>
 	</center>
 	<body></body>
 	<div class = "container">
 		<div class = "row">
 			<div class = "box" width = "70%">
-                <p>Hello. Welcome to my brain. This series of posts is meant to serve as an expression of a subset of my thoughts. This set of thoughts contains observations of the world which surrounds me; attempts to articulate truth; my convictions, beliefs, intentions, and goals as a human; random artistic works; and other thoughts which i think should be made public to any mind which might stumble upon this repository. It's basically just a blog. </p>
-                <p>Over the past several years, I have accumulated many writings in my journal. I intend to sift through those and post the good stuff, with refinements when necessary. I will provide dates with those when possible. I've meant to do this for a while, but had never gotten around to it. And every time I would attempt to start it, I would quickly become overwhelmed with all the possible directions the words could go, not to mention the already existing volume of unorganized previous writings. It seems like a good practice to implement, to improve writing and critical thinking skills. And hey, why not? From an evolutionary perspective, it actually seems like the most sensible thing to do. I'm a guy with a computer, an internet connection, and a brain. Why wouldn't I want to emit my model of the universe into the global mind which now permeates the planet? It seems that by doing this, the main result would simply be further epistomological coherence among the humans with whom I share this world. If you've been on Facebook or looked at popular news sources, you would be forced to believe that the human species has lost any sense of rationality -- maybe we have. I will also post links to other interesting/useful resources and blogs. </p>
-                <p>it certainly seems like we're entering into interesting times: global virus pandemic, violent displays expressing frustration with social institutions, satellite internet, a president sabotoging the mailing system, continued disregard for health of ecosystems. i don't know everything. i don't know which sequence of actions would lead to the best possible outcome, and i don't know what "the best outcome" is. but i feel that there might be a lack of a drive for "sense making". i fear that we might have come to take things for granted a little too much, and may be losing some of our capacity to establish a consensus on what's happening and what we should do about it. the quarantine certainly does not help. but it has provided an opportunity supposedly things are pretty shitty for a lot of people right now. i happen to have it pretty good; i still have a job, a strong immune system, and access to abundant food. however, i am aware that the systems we currently have in place but i can see that the activites of daily human conduct are not in accordance with what would logically follow if it was assumed that the goal is to establish the most ideal situation. 
-				one problem is that we've been trained to assume that society works in a certain way, and we can about and do what we want to do on an individual level as long as we have money to afford it. we don't have to worry about the logistics of the material infrastructure because that's already been taken care of by other smarter people before us. but we are now begin to see that infrastructure become less dependable. i hope that we do not forget what technology should be for. we need to reconnect with nature, or rather, establish a harmonious relationship with it. one that ebbs and flows in a way that doesn't agitate any being. 
+			<center><a href = "http://108.28.164.96/f/IMG_20191210_163302(1).jpg"><img src = "http://108.28.164.96/f/IMG_20191210_163302(1).jpg" width="30%" /></a><br>
+                            <small>Lake Ada Hayen, Ames, IA, December 09, 2019, when the moon was rising while the sun was setting roughly to 180degrees from each other.</small></center>
+                        <br>
+                <p>Hello world. Welcome to the as-of-yet untitled repository of my thoughts and ramblings. Here you will find mostly observations, attempts to articulate what i believe to be true, my convictions and intentions, or random artistic works. </p>
+				<p>Over the past several years, i have accumulated many writings in my journal which I believe to hold some value, or at least spark some thought or discussion. I intend to sift through them and post what is worth sharing. While I have a brain, a computer, an internet connection, and a 9 to 5 job for the time being, I might as well emit my model of the universe into this global mind which now permeates the planet, especially during this time of somewhat of a cultural break-down. One of my good friends has a saying, “When we remain silent, we appear to consent.” There are too many interesting and important things going on and too many ways to potentially make the world a better place to remain silent about it. </p>
+				<p>This first post won’t be that long because I just want to get something out there. I’m back in Virginia after hanging in South Carolina for a month, with a new software engineering job and moved into a single bedroom apartment, which is within walking distance from work. I have the fastest internet I’ve ever had, so I’m excited to host servers and see what else I can do with that. I am currently hosting a modded minecraft (MC Eternal) server that some friends and I have been playing on. </p>
+				<p>The band I’m in, Portal, is finally releasing its 2nd album on December 21st, which happens to coincide with the day that saturn and jupiter will reach their conjunction. If you don’t know about this significant astronomical event, <a href="https://www.space.com/jupiter-saturn-great-conjunction-2020">read about it here</a>. We did not plan that. I wish I could’ve spent more time to record better drum parts, but I had about 6 days last December to record the the entire album before moving from Iowa to Virginia. So I consider the album an EP. The guitar parts ended up being way more polished than the drum parts. I’m excited for it though; it still sounds pretty awesome I think. </p>
+				<p>Here are some topics that I’ve written/thought about and which I will cover in subsequent posts:
+					<ul style = "margin-left: 5rem;">
+						<li>cognition, perception, conscoiusness, intelligence</li>
+						<li>physics, information, evolution, art</li>
+						<li>“alternative” medicine (ayurveda, herbology, yoga)</li>
+						<li>climate change, ecology, natural patterns like domination vs acceptance, taking vs giving, masculine vs feminine, etc.</li>
+						<li>culture, power, time, attention</li>
+						<li>depth psychology</li>
+						<li>random technical things like energy systems, resource management, artifical intelligence</li>
+					</ul>
 				</p>
-
-				<p>
-
-				</p>
-				
-				<p>Something that i was thinking about is how the covid pandemic is going to affect young kids. when i was hiking the other day, i walked past a family with three or four young children. as i approached, the mom told the kids to put on their masks. as one of the kids complained in helpless desparation, "i don't waaaaannaa!", i could see that they've gotten used to the process of turning away from strangers and looking down, in defense against "the virus". i am concerned that there may be negative psychological effects on an entire generation of kids. they are being born into a society where they are taught to keep distance between themselves and others and to put on a mask when around other people. they are taught to fear human interaction by default. they don't get to see the variety of facial expressions. </p>
-				<p>I suppose it would be a good idea to give some context to where I'm coming from with these posts. I'll just give a brief update on my life to end this post, then the next post will be more based off of some previous writings. I've been working and saving money since January. In my free time I've been playing video games, practicing piano and sticking, playing djembe, reading, programming, exercising, hiking. I went to an open mic a few times before the pandemic happened. I test drove two vans so far. I'm trying to decide if I want to get a van or a Subaru forester/ascent. I want something that I can sleep in, for travel. 
-				</p>
-				
-				<p>
-					the purpose of this website is to analyze what i am as a thing. i am truly overwhelmed by what i have at my disposal. 
-				</p>
+                                <p>Here are some piano parts I recorded the past half-year. I have a bad habit of having a good idea for a song then recording some of it then forgetting to touch it again. Feel free to download these tracks, play along, or contact me for further collaboration.
+                                    <audio controls src="http://108.28.164.96/f/sample5.9.ogg"></audio><br><br>
+                                    <audio controls src="http://108.28.164.96/f/panic-piano.ogg">< /audio>
+                                </p>
+				<p>So with that all out of the way, my next post which I’ve already written most of will be about the  existential threat that we’ve been living with for the past few decades that most people are unable to take seriously. How did it get this way? Will technology save us? How can we actually begin to implement the decentralized and harmonious-in-all-domains method of governance which the intelligent-among-us have dreamed of? </p>
+				<center>
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/Dp6LT2MdaPI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				</center>
             </div>
         </div>
     </div>
