192671d13c1133e9f3c3cbf914dd2f9351526d26
grothedev <grothedev@gmail.com>
2021-05-13 01:30:29 -0500

    add cancel restart route

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,7 @@ Route::post('verum/store', 'WritingController@store');
 Route::post('verum/comments/store', 'CommentController@store');
 
 Route::get('mc-vh-restart', 'SiteController@restartMCServer');
-
+Route::get('mc-vh-cancelrestart', 'SiteController@cancelRestartMCServer');
 Route::get('4', function(){
 	return view('4chan_search');
 });
