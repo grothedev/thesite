3185cffd5609891327c73655de3479a280694c26
grothedev <grothedev@gmail.com>
2023-01-02 10:12:39 -0500

    fix syntax

--- resources/views/note.blade.php
+++ resources/views/note.blade.php
@@ -3,7 +3,7 @@
 	<center>
 	<form method = "POST" action = "/n">
 		<input type="hidden" value = {{ $t }} />
-		<textarea name="text" />
+		<textarea name="text"></textarea>
 		<button type = "submit">Append Thought</button>
 	</form>
 	</center>
