d362ff8890c45a2c205fd831b1887170ac6f8811
BuildTools <grothedev@gmail.com>
2023-04-06 23:04:21 -0400

    fix to note page

--- resources/views/note.blade.php
+++ resources/views/note.blade.php
@@ -2,7 +2,7 @@
 <html >
 	<center>
 	<form method = "POST" action = "/n">
-		<input type="hidden" value = {{ $t }} />
+		<input type="hidden" name ="tag" value={{$t}} />
 		<textarea name="text"></textarea>
 		<button type = "submit">Append Thought</button>
 	</form>
